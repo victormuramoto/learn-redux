c06fe271f921a5f3cda6bd61d02ace4b7e2a694b
Victor Muramoto <victor.muramoto@locaweb.com.br>
2017-02-22 11:21:51 -0300

    Video 12 done

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,8 +1,17 @@
 function posts(state=[], action) {
-  console.log('the post will change')
-  console.log(state, action);
-
-  return state;
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+      const i = action.index;
+      console.log('INCREMENTING_LIKES!!!')
+      //return the updated state
+      return [
+        ...state.slice(0,i), //before the one we are updating
+        {...state[i], likes: state[i].likes + 1 },
+        ...state.slice(i + 1 ) //after the one we are updating
+      ]
+    default:
+      return state;
+  }
 }
 
 export default posts;
